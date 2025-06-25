9e8013fa235f0c29c304ef67843d09bc7da79fca
siddhant <32503425+Siddhantt@users.noreply.github.com>
2025-06-26 02:05:13 +0530

    Update CalculatorTest.java

--- src/test/java/com/example/sonar/service/CalculatorTest.java
+++ src/test/java/com/example/sonar/service/CalculatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();

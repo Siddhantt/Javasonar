0e922668d646925ff5e530249578b0c4f41be14b
siddhant <32503425+Siddhantt@users.noreply.github.com>
2025-06-26 02:03:41 +0530

    Create Main.java

---
+++ src/main/java/com/example/sonar/Main.java
@@ -0,0 +1,8 @@
+package com.example.app;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("2 + 3 = " + calc.add(2, 3));
+    }
+}
